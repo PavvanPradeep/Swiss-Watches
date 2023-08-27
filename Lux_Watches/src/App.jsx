1408b95c2300a7bfa3fef4632c616768df55d799
mayankpm <92316639+mayankpm@users.noreply.github.com>
2023-08-27 23:05:45 +0530

    login, sidenav and navbar

--- Lux_Watches/src/App.jsx
+++ Lux_Watches/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import Login from './Components/Login/Login'
+import Navbar from './Components/Sidenav/Navbar'
 import Footer from './Components/Footer/Footer'
 
 export default function App() {
   return (
     <>
-     <h1 className="flex justify-center bg-blue text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <h1 className="flex justify-center bg-blue text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <h1 className="flex justify-center bg-blue text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      {/* <div className='video-container'>
+        <video autoPlay loop muted>
+          <source src='src\assets\The new Rolex Explorer.mp4' type='video/mp4'/>
+        </video>
+      </div> */}
+      <Login/>
+      <Navbar/>
       <Footer/>
     </>
   )
